59395d7a1f012fd5608d0f1b7889ee8bea786f70
Anurag Sharma <blstyle90@gmail.com>
2025-10-25 23:07:12 +0530

    Class 36 till 11:00

---
+++ 08_events/eventbasics.js
@@ -0,0 +1,8 @@
+console.log("Getting started with event");
+
+// iss file ko hum kaam mei bhi nahi lene wale itna
+// sir check kar rahe the sari github se file leke local
+// environment (host) mei hi chala lein sab kuch
+
+// to hum 08_events file ke andar nayi file create karte hai
+// iska naam rakhte hai kuch one.html
