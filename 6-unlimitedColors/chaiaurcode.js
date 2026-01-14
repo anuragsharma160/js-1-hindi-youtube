8bd1691117d277cbfb36dd8cc145e119c19c933c
Anurag Sharma <blstyle90@gmail.com>
2026-01-14 17:19:59 +0530

    Class 38 till 26:52

--- 6-unlimitedColors/chaiaurcode.js
+++ 6-unlimitedColors/chaiaurcode.js
@@ -2,4 +2,24 @@
 
 const randomColor = function(){
     const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color +=hex[Math.floor(Math.random() * 16)]
+    }
+    return color
 }
+
+const startChangingColor = function(){
+
+    setInterval(changeBgColor, 1000)
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor()
+    }
+    
+}
+const stopChangingColor = function(){}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
