cd606cf1938af4bcdc4846082cca260733e14552
Anurag Sharma <blstyle90@gmail.com>
2026-07-19 16:57:54 +0530

    Class 43 till 28:43

--- 10_classes_and_oop/Prototype(2).js
+++ 10_classes_and_oop/Prototype(2).js
@@ -1,3 +1,127 @@
 // 26:40
 
-// 
+// abhi tak jo example liya
+// wo humne kis liye liya tha
+// object ko samajhne ke liye
+// object ke andar new keyword kya
+// hota hai usko samajhne ke liye
+// to wo to aapko samajh mei aa gaya hai
+
+// prototype mai maanta hu thoda sa
+// 10-15% ki kami reh gayi hogi ki
+// thoda aur detail mei agar hum le sakein
+// aur samjha sakein to kaise karenge
+
+// 26:56
+
+// uske liye yeh portion hai please koi
+// timestamp yahan pe add kar dijiyega
+// aa..., ki yahan se humara prototype wala
+// section shuru ho raha hai
+// abhi tak humara new keyword wala section tha
+// to please add kar dijiyega timestamp
+// aap mei se kuch log, kaafi log kar dijiyega
+// isme kya hai, paise thodi lag rahe hai
+// 27:10
+
+// theek hai, to chaliye ab mera goal kya hai
+// aa..., pehle hum goal ke bare mei dekh lete hai
+// kyuki uss goal ke through mai aapko samjhata hua uss goal
+// pe leke aaunga yahan pe
+
+// theek hai to mujhe kya karna hai
+// aa..., mere pas ek goal hai
+// ki mere pas ek string hai let
+// let
+// myName
+// let myName =
+// aur yeh string hai "hitesh"
+// let myName = "hitesh"
+// 27:28
+
+// ab mujhe pata hai ki iss hitesh ( let myName = "hitesh" )
+// ke andar mai kuch console.log kar sakta hu
+// let myName = "hitesh"
+
+// console.log()
+
+// kuch properties mere ko by default milti hai 
+// kyuki yeh ( let myName = "hitesh" ) string hai
+// aa..., dot length property mere ko milti hai
+// let myName = "hitesh"
+
+// console.log(myName.length)
+
+// to length kitni aa rahi hai mere pas
+// achha specific usecase le raha hu
+// taki aapko future mei samajh mei aaye
+// ki kis tareh se libraries banti hai
+// frameworks banti hai
+// kaise aur functionalities add hoti hai
+// low dash etc yeh kaafi popular frameworks
+// libraries hai
+// 27:50
+// yeh kaise banti hai
+
+// to hum chalte hai node ke andar (in terminal)
+// aur humara 10_classes_and_oop ke andar ek prototype hai (prototype.js)
+
+// o/p 6
+
+// to mujhe pata hai 6 (see o/p)
+// letter word hai
+// mujhe pata hai mera naam (sir ka naam)
+// 6 letter ka hai
+
+// Lekin one two three four five
+// (five bar space de diya after last h of "hitesh" in let myName = "hitesh")
+
+// let myName = "hitesh     "
+
+// console.log(myName.length)
+
+// yeh five (five spaces) humne de diya hai
+// ab yeh five humne yahan pe aur add kar diya hai
+// to ab meri length kya hai yahan pe 
+// run in terminal
+
+// o/p 11
+
+// ... to ab meri length kya hai yahan pe 11 (see o/p)
+// jo ki galat baat hai
+// 28:09
+
+// to mai chahta hu ki dot length ( console.log(myName.length) ) 
+// ki tareh hi yahan pe ek property ho jo ki ho
+// truelength
+
+// let myName = "hitesh     "
+
+// console.log(myName.truelength)
+
+// likh liya
+
+// trim vrim nahi
+// haa mujhe pata hai aap trim se hi kaam kar sakte ho
+// but aap fir obvious si baat hai fir aapne
+// wo low IQ wali baatein mat karna ki nahi nahi
+// mai to isse kar dunga usse 
+
+// are aap kuch cheej samajhna chah rahe ho aap
+// hai na ...
+// mujhe pata hai yahan pe koi nahi hai iss channel pe 
+// jo ki low IQ wala hai
+// 28:28
+
+// to yeh truelength ( console.log(myName.truelength) )
+// ek property mai chahta hu
+// yeh ( console.log(myName.truelength) , mei truelength ) property 
+// kya kare ki mujhe console.log de poora 
+// aur poora poora word likh ke de ki 
+// -- truelength of string is 
+// aur uske baad proper value de
+// aur last mei se jitni values hai 
+// yeh sare extra spaces etc
+// ( let myName = "hitesh     " mei hitesh ke last h ke baad ki extra five spaces )
+// hai wo mujhe nikaal ke de
+// 28:43
